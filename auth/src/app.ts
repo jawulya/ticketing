6d5adec396f5b8bcfa35473c9ef1c85bab94b735
Denys Faber <denys.faber@contractors.medallia.com>
2021-05-04 16:46:42 +0300

    change 1

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -9,6 +9,7 @@ import {errorHandler, NotFoundError} from "@dfaber/common";
 import cookieSession from "cookie-session";
 
 const app = express();
+
 app.set('trust proxy', true);
 
 app.use(cookieSession({
